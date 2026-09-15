cbed9927338ba0018aca0b3745507ce9c1f9249e
GabrielSkold <gabriel.skold@hotmail.se>
2026-09-15 13:56:37 +0200

    Add page routing and shared layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { Routes, Route, Router } from "react-router-dom";
 import "./App.css";
+import Overview from "./pages/Overview/Overview";
+import Transactions from "./pages/Transactions/Transactions";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
-    <main>
-      <h1>Balance</h1>
-      <p>Track your income and expenses</p>
-    </main>
+    <>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Overview />} />
+          <Route path="/transactions" element={<Transactions />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 

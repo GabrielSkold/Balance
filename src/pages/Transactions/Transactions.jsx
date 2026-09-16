1bc6352d161860b891e89244b2a9a00673d13f96
GabrielSkold <gabriel.skold@hotmail.se>
2026-09-16 14:23:14 +0200

    Add controlled transaction form with field validation

--- src/pages/Transactions/Transactions.jsx
+++ src/pages/Transactions/Transactions.jsx
@@ -1,9 +1,11 @@
+import TransactionsForm from "../../components/TransactionsForm/TransactionsForm";
 import TransactionsList from "../../components/TransactionsList/TransactionsList";
 
 const Transactions = ({ transactions }) => {
   return (
     <>
       <h1>Transactions</h1>
+      <TransactionsForm />
       <TransactionsList transactions={transactions} />
     </>
   );
